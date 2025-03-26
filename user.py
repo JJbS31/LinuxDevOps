1619e5a0001e729d72429a825708cbb3eb38fb40
JJbS31 <106203775+JJbS31@users.noreply.github.com>
2025-03-26 08:49:32 -0600

    Subiendo mis archivos al repositorio

---
+++ user.py
@@ -0,0 +1,8 @@
+# Programa que pregunta el nombre de la persona e imprime un saludo
+
+def main():
+    nombre = input("Por favor, ingrese su nombre: ")
+    print(f"Hola, {nombre}!")
+
+if __name__ == "__main__":
+    main()
